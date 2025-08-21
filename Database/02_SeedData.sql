fd24b08761fe51444951659ce20e71f403a7fde4
devnishant <ncp1435@gmail.com>
2025-08-21 23:36:33 +0530

    Initial Commit

---
+++ Database/02_SeedData.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO Roles (RoleName, Description)
+VALUES
+  ( 'Admin', 'System administrator with full access'),
+  ( 'Employee', 'Park employees who manage park operations'),
+  ( 'Tourist', 'Tourist users who can explore parks and book safaris');
